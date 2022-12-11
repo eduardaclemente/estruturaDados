c78d31edbfd0df03fa3ce57e851232f63a218763
Eduarda Clemente <eduardaclemente.369@gmail.com>
2022-12-10 22:12:02 -0300

    Explicativa sobre funcionamento de uma atribuição entre tipos e objetos

---
+++ src/main/java/com/dio/atribuicao/Main.java
@@ -0,0 +1,22 @@
+package com.dio.atribuicao;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA= " + intA + " intB= " + intB);
+        intA = 2;
+        System.out.println("intA= " + intA + " intB= " + intA);
+
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA;
+        System.out.println("objA= " + objA + " objB= " + objB);
+        objA.setNum(2);
+        System.out.println("objA= " + objA + " objB= " + objB);
+
+    }
+
+}
